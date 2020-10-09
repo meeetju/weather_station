58334dc6f5584fa654dbf6e8bbe51dca3a6d94c7
meeetju <meeetju@gmail.com>
2020-10-09 23:14:52 +0200

    database

--- __main__.py
+++ __main__.py
@@ -1,8 +1,13 @@
+# encoding: utf-8"
+
+from datetime import date, datetime
 import logging
 import time
 
+
 from _sensors.dht_11 import Dht11
 from _sensors.bmp_280 import Bmp280
+from _database._database import Database, SQL_CREATE_MEASUREMENTS_TABLE, SQL_INSERT_QUERY
 
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
 
@@ -11,15 +16,19 @@ def main():
 
     dht11 = Dht11()
     bmp280 = Bmp280()
-    devices = [dht11, bmp280]
+
+    database = Database('./weather_data.db')
+    database.create_table(SQL_CREATE_MEASUREMENTS_TABLE)
 
     while True:
-         for device in devices:
-             measurements = device.get_measurements()
-             for measurement in measurements:
-                 logging.info(measurement)
-
-         time.sleep(1)
+        dt = datetime.utcnow()
+        humidity = dht11.get_measurements()[0]
+        logging.info(humidity)
+        temperature, pressure = bmp280.get_measurements()
+        logging.info(temperature)
+        logging.info(pressure)
+        database.insert_to_table(SQL_INSERT_QUERY, dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second, humidity.value, temperature.value, pressure.value)
+        time.sleep(5)
 
 
 if __name__ == '__main__':
---
+++ _database/_database.py
@@ -0,0 +1,79 @@
+from contextlib import contextmanager
+import logging
+import sqlite3
+
+
+class Database():
+
+    """Log and get data."""
+
+    def __init__(self, db_file_path):
+        self._db_file_path = db_file_path
+        self._connection = None
+
+    def connect(self):
+        """Create connection to sqlite database."""
+        try:
+            self._connection = sqlite3.connect(self._db_file_path)
+            logging.info('Connection {} established to {}'.format(self._connection, self._db_file_path))
+
+        except sqlite3.Error as error:
+            logging.error('Connection error occurred: {}'.format(error))
+
+    def disconnect(self):
+        """Close connection."""
+        try:
+            self._connection.close()
+            logging.info('Connection closed')
+        except sqlite3.Error as error:
+            logging.error('Connection close error occurred: {}'.format(error))
+
+    @contextmanager
+    def connection(self):
+        """Connect and disconnect from database."""
+        self.connect()
+        yield
+        self.disconnect()
+
+    def create_table(self, sql_create_table):
+        """Create table."""
+        with self.connection():
+            try:
+                cursor = self._connection.cursor()
+                cursor.execute(sql_create_table)
+                logging.info('Table exists or created')
+            except sqlite3.Error as error:
+                logging.error('Table create error occurred: {}'.format(error))
+
+    def insert_to_table(self, sql_insert_template, *args):
+        with self.connection():
+            try:
+                insert = sql_insert_template.format(*args)
+                logging.info('Insert data {}'.format(insert))
+                cursor = self._connection.cursor()
+                cursor.execute(insert)
+                self._connection.commit()
+                logging.info('Inserted data {}'.format(*args))
+                cursor.close()
+            except sqlite3.Error as error:
+                logging.error('Insert error occurred: {}'.format(error))
+
+
+SQL_CREATE_MEASUREMENTS_TABLE = """ 
+CREATE TABLE IF NOT EXISTS measurements (
+year integer,
+month integer,
+day integer,
+hour integer,
+minute integer,
+second integer,
+humidity real,
+temperature real,
+pressure real
+);"""
+
+SQL_INSERT_QUERY = """
+INSERT INTO measurements
+(year, month, day, hour, minute, second, humidity, temperature, pressure) 
+VALUES 
+({},{},{},{},{},{},{},{},{})"""
